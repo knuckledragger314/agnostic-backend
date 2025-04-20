8851fa09576c68f7950d5b9e84db7e45dc90c8ba
knuckledragger314 <charlie.edelen.4@gmail.com>
2025-04-20 14:07:44 -0400

    added update to button_click_processor.py

--- app/button_click_processor.py
+++ app/button_click_processor.py
@@ -1,6 +1,24 @@
+import json
+import boto3
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['BUTTON_CLICKS_TABLE'])
 
 def handler(event, context):
     print("Button click event received:", event)
+    for record in event['Records']:
+        # Decode the Kinesis record
+        payload = json.loads(record['kinesis']['data'])
+
+        # Save data to DynamoDB
+        table.put_item(
+            Item={
+                'id': payload['id'],
+                'timestamp': payload['timestamp'],
+                'clickData': payload['clickData']
+            }
+        )
     return {
         "statusCode": 200,
         "body": "Button click recorded!"
